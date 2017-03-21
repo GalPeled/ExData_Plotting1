98e40dabe990fc26a8f6750cfa23768e6e852baa
GalPeled <visbond@gmail.com>
2017-03-21 22:39:25 +0200

    Add Plot and Script

---
+++ Plot2.R
@@ -0,0 +1,52 @@
+plot2<-function()
+{
+  DownloadAndUnzipAssigmentFileIfNeede()
+  ActivePower <- ReadTable()
+  png(file = "plot2.png", width = 480, height = 480, units = "px")
+  with(ActivePower, plot(Global_active_power ~ datetime, type = "l", 
+                         ylab = "Global active power (kilowatts)",xlab = NA))
+  dev.off()
+}
+
+DownloadAndUnzipAssigmentFileIfNeede<-function()
+{
+  fileName<- "Electric_power_consumption.zip"
+  ##chack to see that we didnt allredy downlode and unzip the file 
+  if(!file.exists(fileName))
+  {
+    fileURL<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileURL,fileName,mode = "wb")
+  }
+  unzip(fileName)
+}
+
+ReadTable<-function()
+{
+  suppressMessages(ValidateLubridatePackage())
+  fileName <-"household_power_consumption.txt"
+  table <- read.csv(fileName,sep = ";",na.strings = "?")
+  table <- table[table$Date == "1/2/2007" | table$Date == "2/2/2007",]
+  table$datetime <- paste(table$Date, table$Time)
+  table$datetime <- dmy_hms(table$datetime)
+  table[,3:9] <- sapply(table[,3:9], as.numeric)
+  return(table)
+}
+
+ValidateLubridatePackage <- function(){
+  list.of.packages <- c("lubridate")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)){
+    print("The lubridate packege is a requierd package for this Function to work")
+    install_Package <- menu(title = 'Should I automaticly install the package now?',choices = c("Yes","No"))
+    if(install_Package)
+    {
+      suppressMessages(install.packages(new.packages))
+    }
+    else
+    {
+      return(FALSE)
+    }
+  }
+  suppressMessages(library(lubridate))
+  return(TRUE)
+} 
---
+++ Plot3.R
@@ -0,0 +1,58 @@
+plot3<-function()
+{
+  DownloadAndUnzipAssigmentFileIfNeede()
+  ActivePower <- ReadTable()
+  png(file = "plot3.png", width = 480, height = 480, units = "px")
+  with(ActivePower, plot(Sub_metering_1 ~ datetime, type = "n", 
+                         ylab = "Energy sub metering",xlab = NA))
+  lines(ActivePower$datetime,ActivePower$Sub_metering_1,col = "black")
+  lines(ActivePower$datetime,ActivePower$Sub_metering_2,col = "red")
+  lines(ActivePower$datetime,ActivePower$Sub_metering_3,col = "blue")
+  legend("topright", 
+         legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+         lty = c(1,1,1), col = c("black", "red", "blue"))
+  dev.off()
+}
+
+DownloadAndUnzipAssigmentFileIfNeede<-function()
+{
+  fileName<- "Electric_power_consumption.zip"
+  ##chack to see that we didnt allredy downlode and unzip the file 
+  if(!file.exists(fileName))
+  {
+    fileURL<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileURL,fileName,mode = "wb")
+  }
+  unzip(fileName)
+}
+
+ReadTable<-function()
+{
+  suppressMessages(ValidateLubridatePackage())
+  fileName <-"household_power_consumption.txt"
+  table <- read.csv(fileName,sep = ";",na.strings = "?")
+  table <- table[table$Date == "1/2/2007" | table$Date == "2/2/2007",]
+  table$datetime <- paste(table$Date, table$Time)
+  table$datetime <- dmy_hms(table$datetime)
+  table[,3:9] <- sapply(table[,3:9], as.numeric)
+  return(table)
+}
+
+ValidateLubridatePackage <- function(){
+  list.of.packages <- c("lubridate")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)){
+    print("The lubridate packege is a requierd package for this Function to work")
+    install_Package <- menu(title = 'Should I automaticly install the package now?',choices = c("Yes","No"))
+    if(install_Package)
+    {
+      suppressMessages(install.packages(new.packages))
+    }
+    else
+    {
+      return(FALSE)
+    }
+  }
+  suppressMessages(library(lubridate))
+  return(TRUE)
+} 
